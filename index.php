6969fc9eb5dbf05784969855d9a6137ef812f42e
OdriDinn <dia-ka2016@yandex.ru>
2023-10-23 20:37:18 +0500

    начало

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include("dbconnect.php");
+ 
+$query = "SELECT * FROM qwerty";
+$result = $mysqli->query($query); //mysql - подключение к базе, query - запрос
+
+
+while ($row = $result->fetch_assoc()) {
+    echo $row["id"] . ': ' . $row["name"] . ': ' . $row["surname"] . ': ' . $row["sex"]; 
+}
+?>
+<form action="dbconnect.php">
+     <input type="submit" value="Перейти на dbconnect" class="button1" /> <!--переход по кнопке на страницу -->
+</form>
