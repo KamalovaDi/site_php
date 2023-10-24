6f1231bd8c1d5e76b3bcf18b43817ac520480677
High_tekk <ben4tech@bk.ru>
2023-10-24 20:56:46 +0500

    шапка 1.3

--- header.php
+++ header.php
@@ -7,11 +7,5 @@
 		<li><a href = "#">О нас</a></li>
 		<li><a href = "#">Услуги</a></li>
 </ul>
-<div class = "title">Название сайта</div>
-<ul class = "menu">
-		<li><a href = "#">Контакты</a></li>
-		<li><a href = "#">О нас</a></li>
-		<li><a href = "#">Услуги</a></li>
-</ul>
 </header>
 </div>
