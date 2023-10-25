0dcd7dcba34b0bec4cb8000971563156c0069cee
High_tekk <ben4tech@bk.ru>
2023-10-25 22:59:58 +0500

    212121

--- header.php
+++ header.php
@@ -5,10 +5,12 @@
 		<a href="index.php"><img src="pg1.jpg" style="height: 10%; width: 10%;"></a>
 	</div>
 <ul class = "menu">
-		<i class="button_login"><li><a href = "#" >Логин</a></li></i>
-		<h4><li><a href = "#" >Выход</a></li></h4>
 		<h4><li><a href = "#" >Каталог</a></li></h4>
 		<h4><li><a href = "#" >Контакты</a></li></h4>
+</ul>
+<ul class = "menu1">
+		<h4><li><a href = "#" >Авторизация</a></li></h4>
+		<h4><li><a href = "#" >Выход</a></li></h4>
 </ul>
 </header>
 </div>
