3840963a1ca94d779cbb216cbdd911fd8f94dd27
High_tekk <ben4tech@bk.ru>
2023-10-25 21:59:47 +0500

    шапка 1.5

--- header.php
+++ header.php
@@ -1,11 +1,14 @@
 <link rel="stylesheet" href="styles.css">
 <div id = "wrap">
 <header class="header1">
-    <div class = "title">Название сайта</div>
+    <div class = "title">
+		<a href="#"><img src="img/pg1.jpg" class="logo"></a>
+	</div>
 <ul class = "menu">
-		<li><a href = "#">Контакты</a></li>
-		<li><a href = "#">О нас</a></li>
-		<li><a href = "#">Услуги</a></li>
+		<li><a href = "#" class="button_login">Логин</a></li>
+		<li><a href = "#" class="button_exit">Выход</a></li>
+		<li><a href = "#" class="button_catalog">Каталог</a></li>
+		<li><a href = "#" class="button_contacts">Контакты</a></li>
 </ul>
 </header>
 </div>
