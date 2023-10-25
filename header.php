3235765b4130691b4f7089c2881d822e9f83004f
High_tekk <ben4tech@bk.ru>
2023-10-25 22:48:07 +0500

    23534

--- header.php
+++ header.php
@@ -2,10 +2,10 @@
 <div id = "wrap">
 <header class="header1">
     <div class = "title">
-		<a href="index.php"><img src="img/pg1.jpg" style="height: 10%; width: 10%;"></a>
+		<a href="index.php"><img src="pg1.jpg" style="height: 10%; width: 10%;"></a>
 	</div>
 <ul class = "menu">
-		<h4><li><a href = "#" >Логин</a></li></h4>
+		<i class="button_login"><li><a href = "#" >Логин</a></li></i>
 		<h4><li><a href = "#" >Выход</a></li></h4>
 		<h4><li><a href = "#" >Каталог</a></li></h4>
 		<h4><li><a href = "#" >Контакты</a></li></h4>
