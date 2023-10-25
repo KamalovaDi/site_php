740754efe1cde306d56b1f215ae89c31d149f391
High_tekk <ben4tech@bk.ru>
2023-10-25 22:17:17 +0500

    1.7

--- header.php
+++ header.php
@@ -5,10 +5,10 @@
 		<a href="index.php"><img src="img/pg1.jpg" style="height: 10%; width: 10%;"></a>
 	</div>
 <ul class = "menu">
-		<li><a href = "#" class="button_login">Логин</a></li>
-		<li><a href = "#" class="button_exit">Выход</a></li>
-		<li><a href = "#" class="button_catalog">Каталог</a></li>
-		<li><a href = "#" class="button_contacts">Контакты</a></li>
+		<h4 class="button_login"><li><a href = "#" >Логин</a></li></h4>
+		<h4 class="button_exit"><li><a href = "#" >Выход</a></li></h4>
+		<h4 class="button_catalog"><li><a href = "#" >Каталог</a></li></h4>
+		<h4 class="button_contacts"><li><a href = "#" >Контакты</a></li></h4>
 </ul>
 </header>
 </div>
