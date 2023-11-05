6e321191e931a1d8f7598744ef25de623f6da004
OdriDinn <dia-ka2016@yandex.ru>
2023-11-05 22:49:29 +0500

    1

--- main1.php
+++ main1.php
@@ -9,41 +9,44 @@ include("header.php");
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
 <div class="search"> 
-<div class="category-wrap1"> 
-    <div class="category-wrap">
-        <h3>Категории</h3>
-        <ul>
-            <li><a href="">Дизайн</a></li>
-            <li><a href="">Фотошоп</a></li>
-            <li><a href="">Книги</a></li>
-            <li><a href="">Журналы</a></li>
-            <li><a href="">Картинки</a></li>
-        </ul>
-    </div>
-</div>
-<div class="row"> 
-    <div class="search_cat_div">
-        <form action="" method="get">
-            <input type="text" placeholder="Искать здесь...">
-            <button class="searchbtn" type="submit"></button>
-        </form>
-    </div>
-    <span class="icon">
-        <i class="fa fa-heart fa-fw"></i>
-        <i class="fa fa-truck fa-fw"></i>
-    </span>
+    <div class="category-wrap1"> 
+        <div class="category-wrap">
+            <h3>Категории</h3>
+            <ul>
+                <li><a href="">Дизайн</a></li>
+                <li><a href="">Фотошоп</a></li>
+                <li><a href="">Книги</a></li>
+                <li><a href="">Журналы</a></li>
+                <li><a href="">Картинки</a></li>
+            </ul>
+        </div>
+        </div>
+    <div class="row"> 
+        <div class="row_1">
+            <div class="search_cat_div">
+                <form action="" method="get">
+                    <input type="text" placeholder="Искать здесь...">
+                    <button class="searchbtn" type="submit"></button>
+                </form>
+            </div>
+            <div class="icon">
+                <i class="fa fa-heart fa-fw"></i>
+                <!-- <i class="fa fa-truck fa-fw"></i> -->
+            </div>
+        </div>
+    
 
-    <div class="hero__item set-bg" data-setbg="banner-1.jpg" 
-    style="background-image: url(&quot;banner-1.jpg&quot;);">
-        <div class="hero__text">
-            <span>FRUIT FRESH</span>
-                 <h2>Vegetable <br>100% Organic</h2>
-                    <p>Free Pickup and Delivery Available</p>
-                     <a href="#" class="primary-btn">SHOP NOW</a>
+        <div class="hero__item set-bg" data-setbg="banner.jpg" 
+        style="background-image: url(&quot;banner.jpg&quot;);">
+            <div class="hero__text">
+                <span>FRUIT FRESH</span>
+                <h2>Vegetable <br>100% Organic</h2>
+                <p>Free Pickup and Delivery Available</p>
+                <a href="#" class="primary-btn">SHOP NOW</a>
+            </div>
         </div>
     </div>
-    </div>
-
+    <!-- </div> -->
 </div>
 
 
